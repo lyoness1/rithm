793df17e153760372e42999f4b43df651028188c
Allison Lyon <allison.lyon@alation.com>
2018-01-23 15:51:48 -0800

    up and down vote buttons

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, jsonify
 import requests
-
-from model import Joke
+import json
 
 app = Flask(__name__)
 
+all_jokes = {}
+
+def _get_random_joke():
+    resp = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'})
+    joke_data = json.loads(resp.text)
+    if joke_data['id'] not in all_jokes:
+        all_jokes[joke_data['id']] = joke_data
+    return joke_data
 
 def _get_jokes():
-    joke = Joke()
-    jokes = set()
+    current_jokes = {}
     for i in range(20):
-        rand_joke = joke.get_random()
-        if rand_joke not in jokes:
-            jokes.add(rand_joke)
-    return jokes
+        rand_joke = _get_random_joke()
+        if rand_joke['id'] not in current_jokes:
+            current_jokes[rand_joke['id']] = rand_joke['joke']
+    return current_jokes
 
 @app.route('/')
 def hello_world():
@@ -22,12 +28,8 @@ def hello_world():
 @app.route('/jokes', methods=['GET'])
 def jokes():
     jokes = _get_jokes()
+    print ("Jokes", jokes)
     return render_template('home.html', jokes=jokes)
-
-# @app.route('/ajax/jokes', methods=['GET'])
-# def get_jokes():
-#     jokes = _get_jokes()
-#     return jsonify(jokes)
 
 
 if __name__ == '__main__':
