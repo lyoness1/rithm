406b5fecf89a8eceb3965d8d9d735dd7d8c4b041
Allison Lyon <allison.lyon@alation.com>
2018-01-23 14:06:15 -0800

    external joke API integration

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+import json
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/jokes', methods=['GET'])
+def get_jokes():
+    resp = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'})
+    joke_data = json.loads(resp.text)
+    joke_text = joke_data['joke']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
